163fb3179f3fa397d1496931f3ac621da8051efd
Wasek Samin <wasek.samin47@gmail.com>
2024-11-07 23:52:59 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: false,
     images: {
-        domains: ['127.0.0.1'],  // Add your local IP address or any other allowed domains here
+        domains: ['127.0.0.1', "ebook.pockethost.io"],  // Add your local IP address or any other allowed domains here
     },
     typescript: {
         ignoreBuildErrors: true,
